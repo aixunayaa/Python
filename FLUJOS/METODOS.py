ea9e3f28cd6c4d1ff47354c63265a441585c36c5
Aixa Naya <aixanaya235@gmail.com>
2025-04-24 14:59:53 -0300

    METODOS.py

--- FLUJOS/METODOS.py
+++ FLUJOS/METODOS.py
@@ -1,5 +1,63 @@
-# METODOS
-colores = ["rojo", "verde", "azul"]
-colores.append("amarillo")
-colores.remove("verde")
-print(colores)
+# Creamos una lista con valores
+lista1 = ['a', 'b', 'c', 'd']
+
+# Añadir o insertar elementos a la lista
+lista1.append('e') # Añade un elemento al final
+print(lista1)
+
+lista1.insert(1, '@') # Inserta un elemento en la posición que le indiquemos como primer argumento
+print(lista1)
+
+lista1.extend(['😃', '😍']) # Agrega elementos al final de la lista
+print(lista1)
+
+# Eliminar elementos de la lista
+lista1.remove('@') # Eliminar la primera aparición de la cadena de texto @
+print(lista1)
+
+ultimo = lista1.pop() # Eliminar el último elemento de la lista y además te lo devuelve
+print(ultimo)
+print(lista1)
+
+lista1.pop(1) # Eliminar el segundo elemento de la lista (es el índice 1)
+print(lista1)
+
+# Eliminar por lo bestia un índice
+del lista1[-1]
+print(lista1)
+
+lista1.clear() # Eliminar todos los elementos de la lista
+print(lista1)
+
+# Eliminar un rango de elementos
+lista1 = ['🐼', '🐨', '🐶', '😿', '🐹']
+del lista1[1:3] # eliminamos los elementos del índice 1 al 3 (no incluye el índice 3)
+print(lista1)
+
+# Más métodos útiles
+print('Ordenar listas modificando la original')
+numbers = [3, 10, 2, 8, 99, 101]
+numbers.sort()
+print(numbers)
+
+print('Ordenar listas creando una nueva lista')
+numbers = [3, 10, 2, 8, 99, 101]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+print("Ordenar una lista de cadenas de texto (todo minúscula)")
+frutas = ['manzana', 'pera', 'limón', 'manzana', 'pera', 'limón']
+sorted_frutas = sorted(frutas)
+print(sorted_frutas)
+
+print("Ordenar una lista de cadenas de texto (mezclas mayúscula y minúscula)")
+frutas = ['manzana', 'Pera', 'Limón', 'manzana', 'pera', 'limón']
+frutas.sort(key=str.lower)
+print(frutas)
+
+# Más cositas útiles
+animals = ['🐶', '🐼', '🐨', '🐶']
+print(len(animals)) # Tamaño de la listas -> 4
+print(animals.count('🐶')) # Cuantas veces aparece el elemento '🐶' -> 2
+print('🐼' in animals) # Comprueba si hay un '🐼' en la lista -> True
+print('🐹' in animals) # -> False
